8597b0afc742cc53d7457f52d0f491c75c239d1e
sharexy <30278614+sharexy@users.noreply.github.com>
2018-04-08 16:43:20 +0800

    Add files via upload
    此处省略Struts2Jar文件的导入，安装框架见JavaWeb/Struts2Basic/

---
+++ Struts2Basic2/src/com/xy/actions/LoginAction.java
@@ -0,0 +1,45 @@
+package com.xy.actions;
+
+import com.opensymphony.xwork2.Action;
+import com.opensymphony.xwork2.ActionContext;
+import com.xy.beans.LoginCheck;
+
+public class LoginAction implements Action {
+	
+	private String username;
+	private String password;
+	
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		
+		LoginCheck check=new LoginCheck();
+		
+		if (check.isLogin(username, password)){
+			ActionContext.getContext().getSession().put("login", "true");
+			
+			return SUCCESS;
+			
+		} else{
+			
+			return ERROR;
+		}
+	}
+
+}
